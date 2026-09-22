5b7ec9c9368a623c4b5b0c46e2c8a8820f731b06
rawan <rewanelkady141@gmail.com>
2026-09-22 13:02:59 +0300

    chore: dist folder only

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
-export default defineConfig({
-  base: '/Coffee-Fancy-Brand/',
-  plugins: [react(), tailwindcss()]
-})
